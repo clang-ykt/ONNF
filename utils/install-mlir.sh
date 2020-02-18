2aa78e00c1ba29d220c5722fc70df04149a543ed
Tian Jin <tjingrant@gmail.com>
2020-02-18 17:04:20 +0800

    Revert unintentional edit.

--- utils/install-mlir.sh
+++ utils/install-mlir.sh
@@ -11,5 +11,5 @@ cmake -G Ninja ../llvm \
    -DLLVM_ENABLE_ASSERTIONS=ON \
    -DLLVM_ENABLE_RTTI=ON
 
-cmake --build . --target
+cmake --build . --target -- ${MAKEFLAGS}
 cmake --build . --target check-mlir
