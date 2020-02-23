4177ac8feb1eb97e1fdf2f1d9a73000e00bfdb7a
Tian Jin <tjingrant@gmail.com>
2020-02-23 21:49:37 +0800

    1. Remove irrelevant code in gen_doc.py

--- doc/gen_doc.py
+++ doc/gen_doc.py
@@ -8,6 +8,7 @@ from collections import defaultdict, OrderedDict
 import io
 import os
 import sys
+import datetime
 
 import numpy as np  # type: ignore
 
@@ -84,26 +85,12 @@ else:
     ext = '.md'
 
 
-def display_number(v):  # type: (int) -> Text
-    if defs.OpSchema.is_infinite(v):
-        return '&#8734;'
-    return Text(v)
-
-
 def should_render_domain(domain):  # type: (Text) -> bool
     if domain == ONNX_ML_DOMAIN and not ONNX_ML:
         return False
     elif ONNX_ML and domain != ONNX_ML_DOMAIN:
         return False
     return True
-
-
-def format_name_with_domain(domain, schema_name):  # type: (Text, Text) -> Text
-    if domain:
-        return '{}.{}'.format(domain, schema_name)
-    else:
-        return schema_name
-
 
 def display_attr_type(v):  # type: (OpSchema.AttrType) -> Text
     assert isinstance(v, OpSchema.AttrType)
@@ -113,165 +100,11 @@ def display_attr_type(v):  # type: (OpSchema.AttrType) -> Text
         s = 'list of ' + s
     return s
 
-
-def display_domain(domain):  # type: (Text) -> Text
-    if domain:
-        return "the '{}' operator set".format(domain)
-    else:
-        return "the default ONNX operator set"
-
-
-def display_domain_short(domain):  # type: (Text) -> Text
-    if domain:
-        return domain
-    else:
-        return 'ai.onnx (default)'
-
-
-def display_version_link(name, version):  # type: (Text, int) -> Text
-    changelog_md = 'Changelog' + ext
-    name_with_ver = '{}-{}'.format(name, version)
-    return '<a href="{}#{}">{}</a>'.format(changelog_md, name_with_ver, name_with_ver)
-
 def get_unique_output_name(schema, name):
     for input in schema.inputs :
         if input.name == name :
             return 'out_'+name
     return name
-
-def display_schema(schema, versions):  # type: (OpSchema, Sequence[OpSchema]) -> Text
-    s = ''
-
-    # doc
-    if schema.doc:
-        s += '\n'
-        s += '\n'.join('  ' + line
-                       for line in schema.doc.lstrip().splitlines())
-        s += '\n'
-
-    # since version
-    s += '\n#### Version\n'
-    if schema.support_level == OpSchema.SupportType.EXPERIMENTAL:
-        s += '\nNo versioning maintained for experimental ops.'
-    else:
-        s += '\nThis version of the operator has been ' + ('deprecated' if schema.deprecated else 'available') + ' since version {}'.format(schema.since_version)
-        s += ' of {}.\n'.format(display_domain(schema.domain))
-        if len(versions) > 1:
-            # TODO: link to the Changelog.md
-            s += '\nOther versions of this operator: {}\n'.format(
-                ', '.join(display_version_link(format_name_with_domain(v.domain, v.name),
-                                               v.since_version) for v in versions[:-1]))
-
-    # If this schema is deprecated, don't display any of the following sections
-    if schema.deprecated:
-        return s
-
-    # attributes
-    if schema.attributes:
-        s += '\n#### Attributes\n\n'
-        s += '<dl>\n'
-        for _, attr in sorted(schema.attributes.items()):
-            # option holds either required or default value
-            opt = ''
-            if attr.required:
-                opt = 'required'
-            elif attr.default_value.name:
-                default_value = helper.get_attribute_value(attr.default_value)
-
-                def format_value(value):  # type: (Any) -> Text
-                    if isinstance(value, float):
-                        formatted = str(np.round(value, 5))
-                        # use default formatting, unless too long.
-                        if (len(formatted) > 10):
-                            formatted = str("({:e})".format(value))
-                        return formatted
-                    elif isinstance(value, (bytes, bytearray)) and sys.version_info[0] == 3:
-                        return str(value.decode('utf-8'))
-                    return str(value)
-
-                if isinstance(default_value, list):
-                    default_value = [format_value(val) for val in default_value]
-                else:
-                    default_value = format_value(default_value)
-                opt = 'default is {}'.format(default_value)
-
-            s += '<dt><tt>{}</tt> : {}{}</dt>\n'.format(
-                attr.name,
-                display_attr_type(attr.type),
-                ' ({})'.format(opt) if opt else '')
-            s += '<dd>{}</dd>\n'.format(attr.description)
-        s += '</dl>\n'
-
-    # inputs
-    s += '\n#### Inputs'
-    if schema.min_input != schema.max_input:
-        s += ' ({} - {})'.format(display_number(schema.min_input),
-                                 display_number(schema.max_input))
-    s += '\n\n'
-    if schema.inputs:
-        s += '<dl>\n'
-        for input in schema.inputs:
-            option_str = ""
-            if OpSchema.FormalParameterOption.Optional == input.option:
-                option_str = " (optional)"
-            elif OpSchema.FormalParameterOption.Variadic == input.option:
-                if input.isHomogeneous:
-                    option_str = " (variadic)"
-                else:
-                    option_str = " (variadic, heterogeneous)"
-            s += '<dt><tt>{}</tt>{} : {}</dt>\n'.format(input.name, option_str, input.typeStr)
-            s += '<dd>{}</dd>\n'.format(input.description)
-        s += '</dl>\n'
-
-    # outputs
-    s += '\n#### Outputs'
-    if schema.min_output != schema.max_output:
-        s += ' ({} - {})'.format(display_number(schema.min_output),
-                                 display_number(schema.max_output))
-    s += '\n\n'
-
-    if schema.outputs:
-        s += '<dl>\n'
-        for output in schema.outputs:
-            option_str = ""
-            if OpSchema.FormalParameterOption.Optional == output.option:
-                option_str = " (optional)"
-            elif OpSchema.FormalParameterOption.Variadic == output.option:
-                if output.isHomogeneous:
-                    option_str = " (variadic)"
-                else:
-                    option_str = " (variadic, heterogeneous)"
-            s += '<dt><tt>{}</tt>{} : {}</dt>\n'.format(get_unique_output_name(schema, output.name), option_str, output.typeStr)
-            s += '<dd>{}</dd>\n'.format(output.description)
-        s += '</dl>\n'
-
-    # type constraints
-    s += '\n#### Type Constraints'
-    s += '\n\n'
-    if schema.type_constraints:
-        s += '<dl>\n'
-        for type_constraint in schema.type_constraints:
-            allowedTypes = type_constraint.allowed_type_strs
-            if (len(allowedTypes) > 0):
-                allowedTypeStr = allowedTypes[0]
-            for allowedType in allowedTypes[1:]:
-                allowedTypeStr += ', ' + allowedType
-            s += '<dt><tt>{}</tt> : {}</dt>\n'.format(
-                type_constraint.type_param_str, allowedTypeStr)
-            s += '<dd>{}</dd>\n'.format(type_constraint.description)
-        s += '</dl>\n'
-
-    # Function Body
-    if schema.has_function:  # type: ignore
-        s += '\n#### Function\n'
-        s += '\nThe Function can be represented as a function.\n'
-
-    return s
-
-
-def support_level_str(level):  # type: (OpSchema.SupportType) -> Text
-    return \
-        "<sub>experimental</sub> " if level == OpSchema.SupportType.EXPERIMENTAL else ""
 
 def convert_type(tstr) :
     tfrom = np.array(['bool', 'int8', 'int16', 'int32', 'int64',
@@ -394,7 +227,6 @@ def gen_schema(schema) :
         return mytype
 
     def get_op_type_for_builder(op_type):
-        print(op_type)
         if op_type.startswith('Variadic'):
             mytype = 'ValueRange'
         else:
@@ -594,157 +426,58 @@ def get_attr_ins(schema):
             name_to_type[attr.name] = get_attr_type_optional(attr_type_str)
     return name_to_type
 
+def build_operator_schemas():
+    # domain -> support level -> name -> [schema]
+    index = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))  # type: Dict[Text, Dict[int, Dict[Text, List[OpSchema]]]]
+    for schema in defs.get_all_schemas_with_history():
+        index[schema.domain][int(schema.support_level)][schema.name].append(schema)
+
+    # Preprocess the Operator Schemas
+    # [(domain, [(support_level, [(schema name, current schema, all versions schemas)])])]
+    operator_schemas = list()  # type: List[Tuple[Text, List[Tuple[int, List[Tuple[Text, OpSchema, List[OpSchema]]]]]]]
+    exsting_ops = set()  # type: Set[Text]
+    for domain, _supportmap in sorted(index.items()):
+        if not should_render_domain(domain):
+            continue
+
+        processed_supportmap = list()
+        for _support, _namemap in sorted(_supportmap.items()):
+            processed_namemap = list()
+            for n, unsorted_versions in sorted(_namemap.items()):
+                versions = sorted(unsorted_versions, key=lambda s: s.since_version)
+                schema = versions[-1]
+                #print("check point 2", schema)
+                if schema.name in exsting_ops:
+                    continue
+                exsting_ops.add(schema.name)
+                processed_namemap.append((n, schema, versions))
+            processed_supportmap.append((_support, processed_namemap))
+        operator_schemas.append((domain, processed_supportmap))
+    return operator_schemas
 
 def main(args):  # type: (Type[Args]) -> None
-    with io.open(args.changelog, 'w', newline='') as fout:
-        fout.write('## Operator Changelog\n')
-        fout.write(
-            "*This file is automatically generated from the\n"
-            "            [def files](/onnx/defs) via [this script](/onnx/defs/gen_doc.py).\n"
-            "            Do not modify directly and instead edit operator definitions.*\n")
-
-        # domain -> version -> [schema]
-        dv_index = defaultdict(lambda: defaultdict(list))  # type: Dict[Text, Dict[int, List[OpSchema]]]
-        for schema in defs.get_all_schemas_with_history():
-            dv_index[schema.domain][schema.since_version].append(schema)
-
-        fout.write('\n')
-
-        for domain, versionmap in sorted(dv_index.items()):
-            if not should_render_domain(domain):
-                continue
-
-            s = '# {}\n'.format(display_domain_short(domain))
-
-            for version, unsorted_schemas in sorted(versionmap.items()):
-                s += '## Version {} of {}\n'.format(version, display_domain(domain))
-                for schema in sorted(unsorted_schemas, key=lambda s: s.name):
-                    name_with_ver = '{}-{}'.format(format_name_with_domain(domain, schema.name),
-                                                   schema.since_version)
-                    s += ('### <a name="{}"></a>**{}**' + (' (deprecated)' if schema.deprecated else '') + '</a>\n').format(name_with_ver, name_with_ver)
-                    s += display_schema(schema, [schema])
-                    s += '\n'
-
-            fout.write(s)
-
     with io.open(args.output, 'w', newline='', encoding="utf-8") as fout:
-        fout.write('## Operator Schemas\n')
-        fout.write(
-            "*This file is automatically generated from the\n"
-            "            [def files](/onnx/defs) via [this script](/onnx/defs/gen_doc.py).\n"
-            "            Do not modify directly and instead edit operator definitions.*\n")
+        curr_utc_time = datetime.datetime.now(datetime.timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
+        autogen_warning = ('//********************************************************\n'
+                          '//   This file is generated on UTC-{}.\n'
+                          '//   Do not modify this file directly.\n'
+                          '//   This file is automatically generated via script.\n'
+                          '//   Details can be found in doc/readonnxdefs.md .\n'
+                          '//********************************************************\n\n')
+        autogen_warning = autogen_warning.format(curr_utc_time)
 
-        # domain -> support level -> name -> [schema]
-        index = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))  # type: Dict[Text, Dict[int, Dict[Text, List[OpSchema]]]]
-        for schema in defs.get_all_schemas_with_history():
-            #print("check point 0", schema.name, schema.domain, schema.support_level)
-            #gen_schema(schema)
-            index[schema.domain][int(schema.support_level)][schema.name].append(schema)
-
-        fout.write('\n')
-
-        # Preprocess the Operator Schemas
-        # [(domain, [(support_level, [(schema name, current schema, all versions schemas)])])]
-        operator_schemas = list()  # type: List[Tuple[Text, List[Tuple[int, List[Tuple[Text, OpSchema, List[OpSchema]]]]]]]
-        exsting_ops = set()  # type: Set[Text]
-        for domain, _supportmap in sorted(index.items()):
-            if not should_render_domain(domain):
-                continue
-
-            processed_supportmap = list()
-            for _support, _namemap in sorted(_supportmap.items()):
-                processed_namemap = list()
-                for n, unsorted_versions in sorted(_namemap.items()):
-                    versions = sorted(unsorted_versions, key=lambda s: s.since_version)
-                    schema = versions[-1]
-                    #print("check point 2", schema)
-                    if schema.name in exsting_ops:
-                        continue
-                    exsting_ops.add(schema.name)
-                    processed_namemap.append((n, schema, versions))
-                processed_supportmap.append((_support, processed_namemap))
-            operator_schemas.append((domain, processed_supportmap))
-
-        # Table of contents
-        for domain, supportmap in operator_schemas:
-            s = '* {}\n'.format(display_domain_short(domain))
-            fout.write(s)
-            function_ops = list()
-            for _, namemap in supportmap:
-                for n, schema, versions in namemap:
-                    if schema.has_function:  # type: ignore
-                        function_ops.append((n, schema, versions))
-                        continue
-                    s = '  * {}<a href="#{}">{}</a>\n'.format(
-                        support_level_str(schema.support_level),
-                        format_name_with_domain(domain, n),
-                        format_name_with_domain(domain, n))
-                    fout.write(s)
-            if len(function_ops):
-                fout.write('\n')
-                fout.write('  **Operators with function registered:**\n')
-                for n, schema, versions in function_ops:
-                    s = '  * {}<a href="#{}">{}</a>\n'.format(
-                        support_level_str(schema.support_level),
-                        format_name_with_domain(domain, n),
-                        format_name_with_domain(domain, n))
-                    fout.write(s)
-
-        fout.write('\n')
         tdfile= io.open(args.tdfile, 'w', newline='') 
-        tdfile.write('//********************************************************\n'+
-                     '//   Warning: Do not modify this file directly\n'+
-                     '//   This file is automatically generated via script\n'+
-                     '//   Details can be found in doc/readonnxdefs.md\n'+
-                     '//********************************************************\n\n'
-               )
+        tdfile.write(autogen_warning)
+
         fefile=io.open('op_build_table.inc', 'w', newline='')
-        firstfunc = True
+        fefile.write(autogen_warning)
 
-        fefile.write('//********************************************************\n'+
-                     '//   Warning: Do not modify this file directly\n'+
-                     '//   This file is automatically generated via script\n'+
-                     '//   Details can be found in doc/readonnxdefs.md\n'+
-                     '//********************************************************\n\n'
-               )
-        # fefile.write('    '+'if (opName == "DUMMY") {\n')
-        for domain, supportmap in operator_schemas:
-            s = '## {}\n'.format(display_domain_short(domain))
-            fout.write(s)
-
+        for domain, supportmap in build_operator_schemas():
             for _, namemap in supportmap:
                 for op_type, schema, versions in namemap:
                     gen_code(schema, fefile)
-
                     r = gen_schema(schema)
                     tdfile.write(r)
-                    s = ('### {}<a name="{}"></a><a name="{}">**{}**' + (' (deprecated)' if schema.deprecated else '') + '</a>\n').format(
-                        support_level_str(schema.support_level),
-                        format_name_with_domain(domain, op_type),
-                        format_name_with_domain(domain, op_type.lower()),
-                        format_name_with_domain(domain, op_type))
-                    
-                    s += display_schema(schema, versions)
-
-                    s += '\n\n'
-
-                    if op_type in SNIPPETS:
-                        s += '#### Examples\n\n'
-                        for summary, code in sorted(SNIPPETS[op_type]):
-                            s += '<details>\n'
-                            s += '<summary>{}</summary>\n\n'.format(summary)
-                            s += '```python\n{}\n```\n\n'.format(code)
-                            s += '</details>\n'
-                            s += '\n\n'
-                    if op_type.lower() in SAMPLE_IMPLEMENTATIONS:
-                        s += '#### Sample Implementation\n\n'
-                        s += '<details>\n'
-                        s += '<summary>{}</summary>\n\n'.format(op_type)
-                        s += '```python\n{}\n```\n\n'.format(SAMPLE_IMPLEMENTATIONS[op_type.lower()])
-                        s += '</details>\n'
-                        s += '\n\n'
-
-                    fout.write(s)
         fefile.close()
 
 
